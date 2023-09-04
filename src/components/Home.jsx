2bcefafdf8d99b089f08cf42a310bb2f84f8be2e
blake-hokanson <blake.hokanson@icloud.com>
2023-09-04 10:30:49 -0500

    Added flip

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,12 +10,36 @@ const Home = () => {
       <div className='home-header'>Welcome to the&nbsp;<i>Functional Codenomics</i>&nbsp;Additional Resources Page!</div>
       
       <div className="circular-menu">
-        <div className="menu-item item-1"><button onClick = {() => navigate('/interaction-codenomics')}>Interaction Codenomics</button></div>
-        <div className="menu-item item-2"><button onClick = {() => navigate('/activity-based-codenomics')}>Activity Based Codenomics</button></div>
-        <div className="menu-item item-3"><button onClick = {() => navigate('/structural-codenomics')}>Structural Codenomics</button></div>
-        <div className="menu-item item-4"><button onClick = {() => navigate('/ptm-codenomics')}>PTM Codenomics</button></div>
-        <div className="menu-item item-5"><button onClick = {() => navigate('/special-codenomics')}>Special Codenomics</button></div>
-        <div className="menu-item item-6"><button onClick = {() => navigate('/temporal-codenomics')}>Temporal Codenomics</button></div>
+
+        <div className="menu-item item-1 card">
+          <img className="imgSize card-front" src={require("../assets/Figure_2.1.png")} alt=""/>
+          <button className="card-back" onClick = {() => navigate('/interaction-codenomics')}>Interaction Codenomics</button>
+        </div>
+
+        <div className="menu-item item-2 card">
+          <img className="imgSize card-front" src={require("../assets/Figure_2.1.png")} alt=""/>
+          <button className="card-back" onClick = {() => navigate('/activity-based-codenomics')}>Activity Based Codenomics</button>
+        </div>
+
+        <div className="menu-item item-3 card">
+          <img className="imgSize card-front" src={require("../assets/Figure_2.1.png")} alt=""/>
+          <button className="card-back" onClick = {() => navigate('/structural-codenomics')}>Structural Codenomics</button>
+        </div>
+
+        <div className="menu-item item-4 card">
+          <img className="imgSize card-front" src={require("../assets/Figure_2.1.png")} alt=""/>
+          <button className="card-back" onClick = {() => navigate('/ptm-codenomics')}>PTM Codenomics</button>
+        </div>
+
+        <div className="menu-item item-5 card">
+          <img className="imgSize card-front" src={require("../assets/Figure_2.1.png")} alt=""/>
+          <button className="card-back" onClick = {() => navigate('/special-codenomics')}>Special Codenomics</button>
+        </div>
+
+        <div className="menu-item item-6 card">
+          <img className="imgSize card-front" src={require("../assets/Figure_2.1.png")} alt=""/>
+          <button className="card-back" onClick = {() => navigate('/temporal-codenomics')}>Temporal Codenomics</button>
+        </div>
 
         <img className="home-logo" src={require('../assets/project_omega_logo.png')} alt="logo" />
       </div>
